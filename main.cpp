8a19a88ee4205ee73980d033c1cd16beb881fefa
Liviu Bouruc <bouruc.liviu2000@gmail.com>
2020-04-03 12:56:26 +0300

    background

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
-#include <bits/stdc++.h>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::Window window(sf::VideoMode(1000, 800), "Flappy Bird");
-
+    sf::RenderWindow window(sf::VideoMode(1000, 800), "Flappy Bird");
+    sf::Texture t; t.loadFromFile("background.png"); sf::Sprite background(t);
     while (window.isOpen())
     {
+        window.clear();
+        window.draw(background);
+        window.display();
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
