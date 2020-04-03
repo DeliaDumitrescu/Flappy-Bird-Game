3c960a933e1ebb6f45cdb637de5bd48280fdb00d
DeliaDumitrescu <deliadum123@gmail.com>
2020-04-03 12:30:13 +0300

    black window

--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
+#include <bits/stdc++.h>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::Window window(sf::VideoMode(1000, 800), "Flappy Bird");
 
     while (window.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
+            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
-        }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        }
     }
-    std::cout<<"good";
+
+
+
+
     return 0;
 }
