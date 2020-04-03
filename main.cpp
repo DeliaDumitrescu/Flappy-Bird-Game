76a94231753d5d2f8c73808d4c615597c2b5ce01
DeliaDumitrescu <deliadum123@gmail.com>
2020-04-03 14:05:47 +0300

    background+bird

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
 #include <SFML/Graphics.hpp>
-
+#include "Bird.h"
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1000, 800), "Flappy Bird");
     sf::Texture t; t.loadFromFile("background.png"); sf::Sprite background(t);
+
     while (window.isOpen())
     {
         window.clear();
         window.draw(background);
+
+        Bird bird;
+        window.draw(bird.get_sprite());
         window.display();
+
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
+                    // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
 
