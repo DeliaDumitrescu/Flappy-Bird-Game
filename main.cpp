96cd92f903de971bab9cf27af71920cf6cdedf33
Delia Ioana Dumitrescu <60919824+DeliaDumitrescu@users.noreply.github.com>
2020-04-06 20:20:24 +0300

    updated

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include "Bird.h"
 #include "Pipes.h"
-
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1000, 800), "Flappy Bird");
     window.setFramerateLimit(360);
 
-    sf::Texture t; t.loadFromFile("Images\\background.png"); sf::Sprite background(t);
+    sf::Texture t;
+    t.loadFromFile("Images//background.png");
+    sf::Sprite background(t);
 
     Bird bird;
     Pipe first, second, third;
@@ -18,24 +19,36 @@ int main()
     {
         window.clear();
         window.draw(background);
+        bird.draw(window);
         for (auto i : { &first,&second,&third }) i->draw(window);
- 
-        window.draw(bird.get_sprite());
-        window.display();
 
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
 
+            if(event.type == sf::Event::MouseButtonPressed)
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    bird.jump();
+                    bird.draw(window);
+                }
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::R)
+                {
+                    //for (auto i : { &first,&second,&third }) i->reset();
+                    bird.reset(); //default
+                }
+            }
         }
+
         for (auto i : { &first,&second,&third }) i->move();
+
+        window.display();
     }
-
-
 
 
     return 0;
